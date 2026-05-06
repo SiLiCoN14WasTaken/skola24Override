4e8135509fdfe5812ef33abcf47a8dfc1bf3d72c
Silicon14 <129602789+SiLiCoN14WasTaken@users.noreply.github.com>
2026-05-06 10:16:30 +0200

    Update fetch URLs in swoverride.js

--- swoverride.js
+++ swoverride.js
@@ -1,8 +1,8 @@
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     Promise.all([
-      fetch("https://raw.githubusercontent.com/SiLiCoN14WasTaken/skola24Override/main/url.txt"),
-      fetch("https://raw.githubusercontent.com/SiLiCoN14WasTaken/skola24Override/main/transform.js")
+      fetch("https://cdn.jsdelivr.net/gh/SiLiCoN14WasTaken/skola24Override/main/url.txt"),
+      fetch("https://cdn.jsdelivr.net/gh/SiLiCoN14WasTaken/skola24Override/main/transform.js")
     ]).then(async ([urlResponse, transformResponse]) => {
       const targetUrl = (await urlResponse.text()).trim();
       if (event.request.url !== targetUrl) return fetch(event.request);
