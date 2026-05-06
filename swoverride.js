5ca6ec6c872dc3ff4267090403523af084af26ca
Silicon14 <129602789+SiLiCoN14WasTaken@users.noreply.github.com>
2026-05-06 10:11:35 +0200

    Update fetch URL for transform.js

--- swoverride.js
+++ swoverride.js
@@ -2,7 +2,7 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     Promise.all([
       fetch("https://raw.githubusercontent.com/user/repo/main/url.txt"),
-      fetch("https://raw.githubusercontent.com/user/repo/main/transform.js")
+      fetch("https://raw.githubusercontent.com/SiLiCoN14WasTaken/skola24Override/main/transform.js")
     ]).then(async ([urlResponse, transformResponse]) => {
       const targetUrl = (await urlResponse.text()).trim();
       if (event.request.url !== targetUrl) return fetch(event.request);
