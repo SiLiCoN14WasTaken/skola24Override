12cb974a007115c8a526a5a705a8c62b862741d1
Silicon14 <129602789+SiLiCoN14WasTaken@users.noreply.github.com>
2026-05-06 10:12:59 +0200

    Update fetch URL in swoverride.js

--- swoverride.js
+++ swoverride.js
@@ -1,7 +1,7 @@
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     Promise.all([
-      fetch("https://raw.githubusercontent.com/user/repo/main/url.txt"),
+      fetch("https://raw.githubusercontent.com/SiLiCoN14WasTaken/skola24Override/main/url.txt"),
       fetch("https://raw.githubusercontent.com/SiLiCoN14WasTaken/skola24Override/main/transform.js")
     ]).then(async ([urlResponse, transformResponse]) => {
       const targetUrl = (await urlResponse.text()).trim();
