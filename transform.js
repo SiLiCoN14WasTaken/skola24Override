21ac196af40cb6b9ea98d46a566c0be08dd213e2
Silicon14 <129602789+SiLiCoN14WasTaken@users.noreply.github.com>
2026-05-06 11:43:59 +0200

    Modify script injection in transform.js

--- transform.js
+++ transform.js
@@ -9,11 +9,14 @@ document.addEventListener('load', function() {
   }
 });
 </script></body>`)*/
-(text) => text.replace("</body>", `<script>
+/*(text) => text.replace("</body>", `<script>
 if (!sessionStorage.getItem('dropdownSet') && window.location.href === 'https://skola24.se/Applications/Authentication/login.aspx?host=skola24.se') {
   sessionStorage.setItem('dropdownSet', '1');
   var dropdown = document.getElementById('DomainDropDown');
   dropdown.value = 1098;
   dropdown.dispatchEvent(new Event('change'));
 }
+</script></body>`)*/
+(text) => text.replace("</body>", `<script>
+window.location.href = 'https://service-sso1.novasoftware.se/saml-2.0/authenticate?customer=http%3a%2f%2ffs.lund.se%2fadfs%2fservices%2ftrust&targetsystem=Skola24';
 </script></body>`)
